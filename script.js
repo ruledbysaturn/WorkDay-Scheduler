3ed97cf6fc54c65b554aa99e0bd45f23a8f0645a
Ruminate <inramiland@gmail.com>
2023-09-24 21:17:52 -0400

    removed dashes from hour id

--- script.js
+++ script.js
@@ -15,30 +15,31 @@ setInterval(upTime, 1000);
 $(function () {
   const currHour= dayjs().format('H');
 //adds the colors to the time blocks
-  function hourColors() {
-    $('.time-block').each(function(){
-      const blockHour = parseInt($(this).attr('id').split('hour')[1]);
-      console.log(blockHour, currHour)
-      if (blockHour < currHour){
-        $(this).addClass('past');
-        $(this).removeClass('present');
-        $(this).removeClass('future');
-      }
-      else if (blockHour === currHour){
-        $(this).removeClass('past');
-        $(this).addClass('present');
-        $(this).removeClass('future');
-      }
-      else {
-        $(this).removeClass('present');
-        $(this).addClass('future');
-        $(this).removeClass('past');
-      }
+function hourColors() {
+  $('.time-block').each(function(){
+    const blockHour = parseInt($(this).attr('id').split('hour')[1]);
+    console.log(blockHour, currHour)
+    if (blockHour < currHour){
+      $(this).addClass('past');
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+    }
+    else if (blockHour == currHour){
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
+    else {
+      $(this).removeClass('present');
+      $(this).removeClass('past');
+      $(this).addClass('future');
+    }
       
     });
   }
   function scheduleEntry() {
     $('.saveBtn').on('click'), function () {
+      console.log(this);
       const hour = $(this).parent().attr('id');
       const text = $(this).siblings('.description').val();
       localStorage.setItem(hour, text);    }
